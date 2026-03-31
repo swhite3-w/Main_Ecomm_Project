588ef46d6d85985ad022fa3371485b945d487709
swhite <swhite3@academic.rrc.ca>
2026-03-31 17:18:16 -0500

    Added validation to the page model.

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,9 @@
 class Page < ApplicationRecord
+
+  validates :slug, presence: true
+  validates :title, presence: true
+  validates :content, presence: true
+
   def self.ransackable_attributes(auth_object = nil)
     ["content", "created_at", "id", "slug", "title", "updated_at"]
   end
