a17b3f739964bdfca84667d8c0765c235f312429
swhite <swhite3@academic.rrc.ca>
2026-03-31 17:14:39 -0500

    Added validation to the category model.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,9 @@
 class Category < ApplicationRecord
   has_many :products, dependent: :destroy
 
+  validates :name, presence: true
+  validates :description, presence: true
+
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "description", "id", "name", "updated_at"]
   end
