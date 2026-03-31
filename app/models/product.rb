807d6827902941deb277113ea6be1a672eb6e879
swhite <swhite3@academic.rrc.ca>
2026-03-31 17:09:56 -0500

    Added validation to the products model.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,13 @@
 class Product < ApplicationRecord
   belongs_to :category
 
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :stock_quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :image_url, presence: true
+  validates :category, presence: true
+
   def self.ransackable_attributes(auth_object = nil)
     ["category_id", "created_at", "description", "id", "image_url", "is_active", "is_featured", "price", "stock_quantity", "title", "updated_at"]
   end
