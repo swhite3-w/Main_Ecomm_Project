2721eab814977900818873f7d19187e8a16c15f4
swhite <swhite3@academic.rrc.ca>
2026-04-13 01:17:16 -0500

    Fixed an error with the store controller caused by a forgotten end tag when discarding debug methods

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,114 +1,114 @@
 class StoreController < ApplicationController
-    def index
-      @categories = Category.order(:name)
-      @products = Product.includes(:category).order(created_at: :desc)
+  def index
+    @categories = Category.order(:name)
+    @products = Product.includes(:category).order(created_at: :desc)
 
-      if params[:keyword].present?
-        keyword = "%#{params[:keyword]}%"
-        @products = @products.where("title LIKE ? OR description LIKE ?", keyword, keyword)
-      end
+    if params[:keyword].present?
+      keyword = "%#{params[:keyword]}%"
+      @products = @products.where("title LIKE ? OR description LIKE ?", keyword, keyword)
+    end
 
-      if params[:category_id].present?
-        @products = @products.where(category_id: params[:category_id])
-      end
+    if params[:category_id].present?
+      @products = @products.where(category_id: params[:category_id])
+    end
 
-      if params[:filter].present?
-        case params[:filter]
-        when "new"
+    if params[:filter].present?
+      case params[:filter]
+      when "new"
         @products = @products.where("created_at >= ?", 3.days.ago)
-        when "recent"
-          @products = @products.where("updated_at >= ?", 3.days.ago)
-                               .where("created_at < ?", 3.days.ago)
-        when "on_sale"
-          @products = @products.where("price < ?", 50)
-        end
-      end
-
-      @products = @products.page(params[:page]).per(14)
-    end
-
-    def show
-      @product = Product.find(params[:id])
-    end
-
-    def category
-      @category = Category.find(params[:id])
-      @categories = Category.order(:name)
-      @products = @category.products.order(created_at: :desc).page(params[:page]).per(14)
-    end
-
-    def about
-      @page = Page.find_by(slug: "about")
-    end
-
-    def contact
-      @page = Page.find_by(slug: "contact")
-    end
-
-    def cart
-      session[:cart] ||= {}
-
-      @cart_items = []
-      @cart_total = 0
-
-      session[:cart].each do |product_id, quantity|
-        product = Product.find_by(id: product_id)
-        next unless product
-
-        quantity = quantity.to_i
-        subtotal = product.price * quantity
-
-        @cart_items << {
-          product: product,
-          quantity: quantity,
-          subtotal: subtotal
-        }
-
-        @cart_total += subtotal
+      when "recent"
+        @products = @products.where("updated_at >= ?", 3.days.ago)
+                             .where("created_at < ?", 3.days.ago)
+      when "on_sale"
+        @products = @products.where("price < ?", 50)
       end
     end
 
-    def add_to_cart
-      session[:cart] ||= {}
+    @products = @products.page(params[:page]).per(14)
+  end
 
-      product_id = params[:id].to_s
-      session[:cart][product_id] ||= 0
-      session[:cart][product_id] += 1
+  def show
+    @product = Product.find(params[:id])
+  end
 
-      product = Product.find(params[:id])
-      flash[:notice] = "#{product.title} was added to your cart."
+  def category
+    @category = Category.find(params[:id])
+    @categories = Category.order(:name)
+    @products = @category.products.order(created_at: :desc).page(params[:page]).per(14)
+  end
 
-      redirect_to cart_path
+  def about
+    @page = Page.find_by(slug: "about")
+  end
+
+  def contact
+    @page = Page.find_by(slug: "contact")
+  end
+
+  def cart
+    session[:cart] ||= {}
+
+    @cart_items = []
+    @cart_total = 0
+
+    session[:cart].each do |product_id, quantity|
+      product = Product.find_by(id: product_id)
+      next unless product
+
+      quantity = quantity.to_i
+      subtotal = product.price * quantity
+
+      @cart_items << {
+        product: product,
+        quantity: quantity,
+        subtotal: subtotal
+      }
+
+      @cart_total += subtotal
     end
+  end
 
-    def update_cart
-      session[:cart] ||= {}
+  def add_to_cart
+    session[:cart] ||= {}
 
-      product_id = params[:id].to_s
-      quantity = params[:quantity].to_i
+    product_id = params[:id].to_s
+    session[:cart][product_id] ||= 0
+    session[:cart][product_id] += 1
 
-      if quantity > 0
-        session[:cart][product_id] = quantity
-        flash[:notice] = "Cart updated."
-      else
-        session[:cart].delete(product_id)
-        flash[:notice] = "Item removed from cart."
-      end
+    product = Product.find(params[:id])
+    flash[:notice] = "#{product.title} was added to your cart."
 
-      redirect_to cart_path
-    end
+    redirect_to cart_path
+  end
 
-    def remove_from_cart
-      session[:cart] ||= {}
+  def update_cart
+    session[:cart] ||= {}
 
-      product_id = params[:id].to_s
+    product_id = params[:id].to_s
+    quantity = params[:quantity].to_i
+
+    if quantity > 0
+      session[:cart][product_id] = quantity
+      flash[:notice] = "Cart updated."
+    else
       session[:cart].delete(product_id)
-
       flash[:notice] = "Item removed from cart."
-      redirect_to cart_path
     end
 
-    def checkout
+    redirect_to cart_path
+  end
+
+  def remove_from_cart
+    session[:cart] ||= {}
+
+    product_id = params[:id].to_s
+    session[:cart].delete(product_id)
+
+    flash[:notice] = "Item removed from cart."
+    redirect_to cart_path
+  end
+
+  def checkout
     session[:cart] ||= {}
 
     @cart_items = []
@@ -134,15 +134,14 @@ class StoreController < ApplicationController
   end
 
   def place_order
-  session[:cart] ||= {}
+    session[:cart] ||= {}
 
-  if session[:cart].empty?
-    flash[:alert] = "Your cart is empty."
-    redirect_to cart_path
-    return
-  end
+    if session[:cart].empty?
+      flash[:alert] = "Your cart is empty."
+      redirect_to cart_path
+      return
+    end
 
-  begin
     province = Province.find(params[:province_id])
 
     customer = Customer.find_or_create_by!(email: params[:customer_email]) do |c|
@@ -206,10 +205,9 @@ class StoreController < ApplicationController
     session[:cart] = {}
     flash[:notice] = "Order #{order.id} placed successfully."
     redirect_to order_confirmation_path(order)
-    end
+  end
 
   def order_confirmation
     @order = Order.find(params[:id])
   end
-
 end
