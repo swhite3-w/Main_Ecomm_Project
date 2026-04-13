33ebbfd5551e3fb1df58565b794d874d0125acbc
swhite <swhite3@academic.rrc.ca>
2026-04-12 22:52:39 -0500

    Added relationship and validations to province model

--- app/models/province.rb
+++ app/models/province.rb
@@ -1,2 +1,9 @@
 class Province < ApplicationRecord
+  has_many :customers, dependent: :nullify
+
+  validates :name, presence: true
+  validates :code, presence: true
+  validates :gst_rate, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :pst_rate, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :hst_rate, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
